b4c4e9e12070e5756186477117b871a2ebe6256d
User <User@DESKTOP-PI3USKV>
2023-11-07 11:52:48 -0500

    Add project files.

---
+++ Numero/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+//ingrese un numero y muestre la tabla de multiplicar
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un Numero");
+        int numero = int.Parse(Console.ReadLine());
+        Console.WriteLine("la tabla de multiplicar es:" + numero+"correcto");
+        for (int i = 1; i <= 10; i++)
+            
+        {
+            int resul = i * numero;
+            Console.WriteLine("el resultado es:" + resul);
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
